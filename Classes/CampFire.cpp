f5e38eb6c6b3582405cd30bdd4a59e0048368b40
Yemanden <V1997V@rambler.ru>
2018-04-27 03:11:07 +0300

    little change in campfire.cpp

--- Classes/CampFire.cpp
+++ Classes/CampFire.cpp
@@ -2,14 +2,21 @@
 
 USING_NS_CC;
 
-/*CampFire::CampFire(FireInfo fire)
-{
+CampFire::CampFire() {}
 
-};*/
+CampFire* CampFire::createWithName(std::string key) {
+	CampFire* _campfire = new (std::nothrow) CampFire();
+	if (_campfire && initWithName(key))
+	{
+		_campfire->autorelease();
+		return _campfire;
+	}
+	CC_SAFE_DELETE(_campfire);
+	return nullptr;
+}
 
-CampFire* CampFire::createWithName(std::string key)
-{
-	_node = Node::create();
-	//Нужны данные о кострах
-	return this;
+bool CampFire::initWithName(std::string key) {
+	FireInfo _info = ObjectManager::getInstance()->getCampfireTemplate(key);
+	_sprite = Sprite::createWithSpriteFrameName(_info.spriteName);
+	//_animates
 }
